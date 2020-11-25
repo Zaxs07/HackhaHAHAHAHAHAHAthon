0d3480d5a958c200f65614fb4d63b66047c2a9b1
GDIMOKS <57040046+GDIMOKS@users.noreply.github.com>
2020-11-26 02:15:14 +0300

    Add bullet

---
+++ Assets/Scripts/BulletPackGet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPackGet : MonoBehaviour
+{
+    public float speedBull = 600f;
+    public Rigidbody bullet;
+    private float lifeTime = 0f;
+    public Vector3 movement;
+    private bool shoot;
+
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        lifeTime += Time.deltaTime;
+
+        transform.Translate(Vector3.forward * speedBull * Time.deltaTime);
+
+        if (lifeTime >= 10f)
+        {
+            Destroy(gameObject);
+            lifeTime = 0f;
+        }
+    }
+}
