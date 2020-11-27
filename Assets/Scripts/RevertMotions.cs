d8243aee7f1ada542848a290e6f33f6f8a73ba96
GDIMOKS <57040046+GDIMOKS@users.noreply.github.com>
2020-11-27 06:42:50 +0300

    New things

---
+++ Assets/Scripts/RevertMotions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevertMotions : MonoBehaviour
+{
+    public moveChairsTrue MCT;
+    public float normHor;
+    public float normVer;
+    private float time;
+    public bool On;
+
+    void Start()
+    {
+        MCT = GameObject.FindWithTag("Player").GetComponent<moveChairsTrue>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        if (On == true)
+        {
+            MCT.Lock = true;
+            if (time <= 5f)
+            {
+                time += Time.deltaTime;
+                MCT.moveHorizontal = Input.GetAxis("Vertical");
+                MCT.moveVertical = Input.GetAxis("Horizontal");
+            }
+            else
+            {
+                MCT.moveHorizontal = Input.GetAxis("Horizontal");
+                MCT.moveVertical = Input.GetAxis("Vertical");
+                On = false;
+                MCT.Lock = false;
+                time = 0f;
+            }
+        }
+    }
+}
--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -4,19 +4,51 @@ using UnityEngine;
 
 public class Trigger : MonoBehaviour
 {
-    private SphereCollider scol;
-    private float cubeLife = 0f;
     private float cubeRespawn = 20f;
+    private float BPRespawn = 10f;
+    private float SPRespawn = 30f;
+    private float BotPRespawn = 15f;
     public int count;
-    public GameObject cube;
-    public Transform cubeSpawn;
+
+    
+    private bool spawn = false;
 
     public GameObject BP;
-    public Transform BPSpawn;
-    private float BPRespawn = 10f;
+    public GameObject shmolik;
+    public GameObject SP;
+    public GameObject BoP;
+
+    public Transform Spawn1;
+    public Transform Spawn2;
+    public Transform Spawn3;
+    public Transform shmolikSpawn1;
+    public Transform shmolikSpawn2;
+
     void Start()
     {
-        
+        if (count == 2)
+        {
+            Instantiate(BP, Spawn1.position, Spawn1.rotation);
+            Instantiate(BP, Spawn2.position, Spawn2.rotation);
+            Instantiate(BP, Spawn3.position, Spawn3.rotation);
+            BPRespawn = 0f;
+        }
+
+        if (count == 3)
+        {
+            Instantiate(SP, Spawn1.position, Spawn1.rotation);
+            Instantiate(SP, Spawn2.position, Spawn2.rotation);
+            Instantiate(SP, Spawn3.position, Spawn3.rotation);
+            SPRespawn = 0f;
+        }
+
+        if (count == 4)
+        {
+            Instantiate(BoP, Spawn1.position, Spawn1.rotation);
+            Instantiate(BoP, Spawn2.position, Spawn2.rotation);
+            Instantiate(BoP, Spawn3.position, Spawn3.rotation);
+            BotPRespawn = 0f;
+        }
     }
 
     // Update is called once per frame
@@ -29,6 +61,10 @@ public class Trigger : MonoBehaviour
     {
         cubeRespawn += Time.deltaTime;
         BPRespawn += Time.deltaTime;
+        SPRespawn += Time.deltaTime;
+        BotPRespawn += Time.deltaTime;
+
+        
     }
 
     private void OnTriggerEnter(Collider col)
@@ -37,18 +73,42 @@ public class Trigger : MonoBehaviour
         {
             if (cubeRespawn > 20f && count == 1)
             {
-                Instantiate(cube, cubeSpawn.position, cubeSpawn.rotation);
+                Instantiate(shmolik, shmolikSpawn1.position, shmolikSpawn1.rotation);
+                Instantiate(shmolik, shmolikSpawn2.position, shmolikSpawn2.rotation);
                 cubeRespawn = 0f;
             }
 
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("OtherPlayers"))
+        {
             if (count == 2 && BPRespawn > 10F)
             {
-                Instantiate(BP, BPSpawn.position, BPSpawn.rotation);
+                Instantiate(BP, Spawn1.position, Spawn1.rotation);
+                Instantiate(BP, Spawn2.position, Spawn2.rotation);
+                Instantiate(BP, Spawn3.position, Spawn3.rotation);
                 BPRespawn = 0f;
             }
+
+            if (count == 3 && SPRespawn > 30f)
+            {
+                Instantiate(SP, Spawn1.position, Spawn1.rotation);
+                Instantiate(SP, Spawn2.position, Spawn2.rotation);
+                Instantiate(SP, Spawn3.position, Spawn3.rotation);
+                SPRespawn = 0f;
+            }
+
+            if (count == 4 && BotPRespawn > 15f)
+            {
+                Instantiate(BoP, Spawn1.position, Spawn1.rotation);
+                Instantiate(BoP, Spawn2.position, Spawn2.rotation);
+                Instantiate(BoP, Spawn3.position, Spawn3.rotation);
+                BotPRespawn = 0f;
+            }
         }
-        
-        
-        
     }
 }
